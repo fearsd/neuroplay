1ae0820f015f6e69eb700cc6518e18a7b2c68178
fearsd <rifatfazlutdinov@gmail.com>
2022-10-08 22:46:08 +0300

    pdf download

---
+++ static/index.js
@@ -0,0 +1,131 @@
+window.jsPDF = window.jspdf.jsPDF;
+document.querySelector(".theme-btn").addEventListener("click", function () {
+  document
+    .querySelectorAll("body")
+    .forEach((l) => l.classList.toggle("page-black"));
+  document
+    .querySelectorAll("textarea")
+    .forEach((l) => l.classList.toggle("field-black"));
+  document
+    .querySelectorAll("input")
+    .forEach((l) => l.classList.toggle("field-black"));
+  document
+    .querySelectorAll("h2")
+    .forEach((l) => l.classList.toggle("page-black"));
+});
+tinymce.init({
+  selector: "textarea#neuroplay-text",
+  toolbar:
+    "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+});
+const login = (code) => {
+  $.ajax({
+    url: "/login",
+    type: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify({ code: code }),
+    success: function (result) {
+      if (result.success) {
+        $(".btn-text").prop("disabled", false);
+      } else {
+        console.log("NE SUETA NE KRUTO");
+      }
+    },
+  });
+};
+
+const saveScenario = (scenario) => {
+  $.ajax({
+    url: "/scenario",
+    type: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify({ scenario: scenario }),
+    success: function (result) {
+      console.log("huy", result);
+    },
+  });
+};
+
+const restoreScenario = () => {
+  $.ajax({
+    url: "/scenario",
+    type: "GET",
+    dataType: "json",
+    contentType: "application/json",
+    success: function (result) {
+      tinymce.activeEditor.setContent(result.scenario);
+    },
+  });
+};
+
+const runContinue = (replic) => {
+  $.ajax({
+    url: "/continue",
+    type: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify({ replic: replic }),
+    success: function (result) {
+      $("#continue-text").val(result.replic);
+    },
+  });
+};
+
+const runResponse = (replic) => {
+  $.ajax({
+    url: "/response",
+    type: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify({ replic: replic }),
+    success: function (result) {
+      $("#response-text").val(result.replic);
+    },
+  });
+};
+
+$(function () {
+  $("#code-btn").click(function (e) {
+    const code = $("#code-input").val();
+    login(code);
+  });
+
+  $("#save-btn").click(function (e) {
+    tinymce.triggerSave();
+    const scenario = tinymce.activeEditor.getContent();
+    saveScenario(scenario);
+  });
+  $("#restore-btn").click(function (e) {
+    tinymce.triggerSave();
+    restoreScenario();
+  });
+  $("#download-btn").click(function (e) {
+    tinymce.triggerSave();
+    let doc = new jsPDF();
+    doc.html(tinymce.activeEditor.getContent(), {
+        callback: function (doc) {
+          doc.save();
+        },
+    });
+  });
+
+  $("#clear-btn-continue").click(function (e) {
+    $("#continue-text-input").val("");
+    $("#continue-text").val("");
+  });
+  $("#clear-btn-response").click(function (e) {
+    $("#response-text-input").val("");
+    $("#response-text").val("");
+  });
+
+  $("#run-btn-continue").click(function (e) {
+    const replic = $("#continue-text-input").val();
+    runContinue(replic);
+  });
+  $("#run-btn-response").click(function (e) {
+    const replic = $("#response-text-input").val();
+    runResponse(replic);
+  });
+});
