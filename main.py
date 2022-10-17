4a2693a6af2b279e12b321f5e873c44aeddf616f
fearsd <rifatfazlutdinov@gmail.com>
2022-10-17 23:08:08 +0300

    fd

---
+++ main.py
@@ -0,0 +1,143 @@
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from werkzeug.exceptions import NotFound
+from finetune.finetune import fineclass
+from finetune.finetune import replyclass
+
+from flask import session
+import pandas as pd
+
+app = Flask(__name__)
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+bootstrap = Bootstrap(app)
+db.init_app(app)
+
+
+def cont(arr, inp):
+    ser = pd.Series(arr)
+    ser = ser.str.strip()
+    ser = ser.str.replace('--', '—')
+    ser = (ser.index + 1).astype('string') + '. ' + ser
+    ans = []
+    for val in ser.values:
+        while(val[-1] not in ['.', '!', '?']):
+            val = val[:-1]
+        ans.append(val + '\n')
+    return ans
+
+
+def rep(arr, inp):
+    ser = pd.Series(arr)
+    ser = ser.str.strip()
+    ser = ser.str[5:]
+    ser = ser.str.strip()
+    ser = ser.str[len(inp):]
+    ser = ser.str.strip()
+    ser = ser.str[6:]
+    ser = ser.str.strip()
+    ser = ser.str.replace('--', '—')
+    ser = (ser.index + 1).astype('string') + '. ' + inp + '\n' + ser
+    ans = []
+    for val in ser.values:
+        while(val[-1] not in ['.', '!', '?']):
+            val = val[:-1]
+        ans.append(val + '\n')
+    return ans
+
+def filt_gpt(text):
+    if ('выход:' in text):
+        arr = text.split('выход:')
+        text = ' '.join(arr[1: ])
+    
+    while ('ютуб' in text):
+        text = text.replace('ютуб', '')
+        
+    alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 
+                'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'э',
+                'ю', 'я', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 
+                'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш',
+                'Щ', 'Э', 'Ю', 'Я']
+
+    while (text[0] not in alphabet):
+        if (len(text) == 1):
+            return 'Что-то пошло не так. Пожалуйста попробуйте сызнова.'
+        text = text[1:]
+    
+    while ('--' in text):
+        text = text.replace('--', '—')
+    
+    return text
+
+class Saving(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    string = db.Column(db.String, unique=True, nullable=True)
+
+with app.app_context():
+    db.create_all()
+    try:
+        saving = db.get_or_404(Saving, 1)
+    except NotFound:
+        saving = Saving(
+            string='',
+        )
+        db.session.add(saving)
+        db.session.commit()
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    response = request.get_json()
+    if response['code'] == 'театр':
+
+        return jsonify({'success': True})
+    return jsonify({'success': False})
+
+
+
+@app.route('/scenario', methods=['GET', 'POST'])
+def scenario():
+    saving = db.get_or_404(Saving, 1)
+    if request.method == 'POST':
+        response = request.get_json()
+        if response['scenario']:
+            saving.string = response['scenario']
+            db.session.add(saving)
+            db.session.commit()
+            return jsonify({'scenario': saving.string})
+        else:
+            return jsonify({'mess': 'no scenario was provided'})
+    return jsonify({'scenario': saving.string})
+
+@app.route('/continue', methods=['POST'])
+def replic_continue():
+    response = request.get_json()
+    print(response)
+    #if response['replic']:
+    #    return jsonify({'replic': response['replic']})
+    if response['replic']:
+        text = fineclass('{0}'.format(response['replic']))
+        print(text)
+        output_text = cont(text, response['replic'])
+        return jsonify({
+            'replic': '\n'.join(output_text)
+        })
+
+@app.route('/response', methods=['POST'])
+def replic_response():
+    response = request.get_json()
+    if response['replic']:
+        text = replyclass('вход:\n{0}выход:\n'.format(response['replic']))
+        print(text)
+        output_text = rep(text, response['replic'])
+        return jsonify({
+            'replic': '\n'.join(output_text)
+        })
+    
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5555, debug=True)
---
+++ static/index.js
@@ -0,0 +1,374 @@
+window.jsPDF = window.jspdf.jsPDF;
+document.querySelector(".theme-btn").addEventListener("click", function () {
+  document
+    .querySelectorAll("body")
+    .forEach((l) => l.classList.toggle("page-black"));
+  document
+    .querySelectorAll("textarea")
+    .forEach((l) => l.classList.toggle("field-black"));
+  document
+    .querySelectorAll("input")
+    .forEach((l) => l.classList.toggle("field-black"));
+  document
+    .querySelectorAll("h4")
+    .forEach((l) => l.classList.toggle("page-black"));
+    $(".huy").attr('style', 'background-color: #262626;');
+
+
+});
+
+count=0;
+$(function () {
+  $('.theme-btn').on('click' ,function(event) {
+    count++;
+    if(count%2==1){
+      //alert($.cookie('save'));
+      $(".huy").attr('style', 'background-color: #262626; color: #b3b3ba;');
+
+      a=tinymce.activeEditor.getContent();
+      tinymce.activeEditor.destroy();
+
+      tinymce.init({
+    selector: "textarea#neuroplay-text",
+    content_css: 'dark',
+    skin: 'tinymce-5-dark',
+    plugins: 'advlist anchor autolink charmap code codesample directionality fullscreen help image insertdatetime link lists media nonbreaking pagebreak preview searchreplace table template toc visualblocks visualchars wordcount',
+    toolbar:"undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | numlist bullist outdent indent | removeformat fullscreen",
+      
+    templates: [
+      {title: 'Some title', description: 'Some description', content: 'Some content'},
+       ],
+   });
+
+      tinymce.activeEditor.setContent(a);
+  }
+    else{
+      $(".huy").removeAttr('style', 'background-color: #262626; color: #b3b3ba;');   
+
+
+      a=tinymce.activeEditor.getContent();
+      tinymce.activeEditor.destroy();
+
+      tinymce.init({
+    selector: "textarea#neuroplay-text",
+    content_css: 'default',
+    plugins: 'advlist anchor autolink charmap code codesample directionality fullscreen help image insertdatetime link lists media nonbreaking pagebreak preview searchreplace table template toc visualblocks visualchars wordcount',
+    toolbar:"undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | numlist bullist outdent indent | removeformat fullscreen",
+      
+    templates: [
+      {title: 'Some title', description: 'Some description', content: 'Some content'},
+       ],
+   });
+
+      tinymce.activeEditor.setContent(a); 
+  }
+  });
+});
+
+
+//alert($.cookie("save2"));
+//$.cookie('save',"huy");
+/*
+tinymce.init({
+  selector: "textarea#neuroplay-text",
+  content_css: 'default',
+  toolbar:
+    "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+});
+*/
+tinymce.init({
+    selector: "textarea#neuroplay-text",
+    content_css: 'default',
+    plugins: 'advlist anchor autolink charmap code codesample directionality fullscreen help image insertdatetime link lists media nonbreaking pagebreak preview searchreplace table template toc visualblocks visualchars wordcount',
+    toolbar:"undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | numlist bullist outdent indent | removeformat fullscreen",
+      
+    templates: [
+      {title: 'Some title', description: 'Some description', content: 'Some content'},
+       ],
+   });
+/*
+if($.cookie("save")!="undefined" || $.cookie("save")!=""){
+tinymce.activeEditor.setContent($.cookie("save2"));
+}
+*/
+
+
+const login = (code) => {
+  $.ajax({
+    url: "/login",
+    type: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify({ code: code }),
+    success: function (result) {
+      if (result.success) {
+        $(".btn-text").prop("disabled", false);
+        $(".btn").attr('style', 'cursor: pointer;'); 
+        $(".code").addClass('is-valid');
+      } else {
+        console.log("NE SUETA NE KRUTO");
+        $('#code-input').val('');
+        alert('Вы ввели неверный пароль! Спробуйте сызнова');
+
+      }
+    },
+  });
+};
+
+const saveScenario = (scenario) => {
+  $(".btn-suc").addClass('saved');
+  //$("#save-btn").html('Hi there!');
+  $.ajax({
+    url: "/scenario",
+    type: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify({ scenario: scenario }),
+    success: function (result) {
+      console.log("huy", result);
+    },
+  });
+  setTimeout(function () {
+$(".btn-suc").removeClass('saved');
+}, 1000); 
+  
+  
+};
+
+const restoreScenario = () => {
+  $.ajax({
+    url: "/scenario",
+    type: "GET",
+    dataType: "json",
+    contentType: "application/json",
+    success: function (result) {
+      tinymce.activeEditor.setContent(result.scenario);
+    },
+  });
+};
+
+const runContinue = (replic) => {
+  var data = new FormData(form);
+  var output = "";
+  for (const entry of data) {
+    output = entry[0] + "=" + entry[1] + "\r";
+  };
+  if (output.toString().includes('continue')){
+  //alert(output);
+
+$('#run-btn-continue').text('Загрузка...')
+  $.ajax({
+    url: "/continue",
+    type: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify({ replic: replic }),
+    success: function (result) {
+      $("#continue-text").val(result.replic);
+      $('#run-btn-continue').text('Запустить')
+    },
+    error: function(result) {
+      console.log(result)
+    }
+  });
+}else{
+
+$('#run-btn-continue').text('Загрузка...')
+  $.ajax({
+    url: "/response",
+    type: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify({ replic: replic }),
+    success: function (result) {
+      $("#continue-text").val(result.replic);
+      $('#run-btn-continue').text('Запустить')
+    },
+    error: function(result) {
+      console.log(result)
+    }
+  });
+
+}
+};
+
+const runResponse = (replic) => {
+  var data = new FormData(form);
+  var output = "";
+  for (const entry of data) {
+    output = entry[0] + "=" + entry[1] + "\r";
+  };
+  if (output.toString().includes('response')){
+  alert(output);
+
+
+  $('#run-btn-continue').text('Загрузка...')
+  $.ajax({
+    url: "/response",
+    type: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify({ replic: replic }),
+    success: function (result) {
+      $("#response-text").val(result.replic);
+      $('#run-btn-continue').text('Запустить')
+    },
+    error: function(result) {
+      console.log(result)
+    }
+  });
+}
+};
+
+
+var form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+  alert('2');
+  var data = new FormData(form);
+  var output = "";
+  for (const entry of data) {
+    output = entry[0] + "=" + entry[1] + "\r";
+  };
+  alert(data);
+  Console.log(output);
+}, false);
+
+
+
+$(function () {
+  $("#code-btn").click(function (e) {
+    const code = $("#code-input").val();
+    login(code);
+  });
+
+  $("#save-btn").click(function (e) {
+    tinymce.triggerSave();
+    const scenario = tinymce.activeEditor.getContent();
+    saveScenario(scenario);
+  });
+  $("#restore-btn").click(function (e) {
+    tinymce.triggerSave();
+    restoreScenario();
+  });
+  $("#download-btn").click(function (e) {
+
+    function binaryAgent(str) {
+      var binString = '';
+      str.split(' ').map(function(bin) {
+          binString += String.fromCharCode(parseInt(bin, 2));
+        });
+      return binString;
+    }
+
+    function Export2Word(element){
+      var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+      var postHtml = "</body></html>";
+      var html = preHtml+tinymce.activeEditor.getContent()+postHtml;
+
+      var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+
+      var downloadLink = document.createElement("a");
+
+    document.body.appendChild(downloadLink);
+    downloadLink.href = url;
+        downloadLink.download = 'текст пьесы.doc';
+        
+        downloadLink.click();
+    
+    document.body.removeChild(downloadLink);
+      
+}
+Export2Word(tinymce.activeEditor.getContent());
+    /*
+    tinymce.triggerSave();
+    window.jsPDF = window.jspdf.jsPDF;
+
+    b=tinymce.activeEditor.getContent();
+    alert(b);
+
+
+    let doc = new jsPDF();
+    doc.html("huy <p> pizda</p> и залупа слоника", {
+        callback: function (doc) {
+          doc.save();
+        },
+    });
+
+*/
+  });
+
+  $("#clear-btn-continue").click(function (e) {
+    $("#continue-text-input").val("");
+    $("#continue-text").val("");
+  });
+  $("#clear-btn-response").click(function (e) {
+    $("#response-text-input").val("");
+    $("#response-text").val("");
+  });
+
+  $("#run-btn-continue").click(function (e) {
+    const replic = $("#continue-text-input").val();
+    runContinue(replic);
+  });
+  $("#run-btn-response").click(function (e) {
+    const replic = $("#response-text-input").val();
+    runResponse(replic);
+  });
+});
+
+
+/*
+
+function setCookie(name, value, options) {
+      options = options || {};
+      var expires = options.expires;
+
+      if (typeof expires == "number" && expires) {
+          var d = new Date();
+          d.setTime(d.getTime() + expires * 1000);
+          expires = options.expires = d;
+      }
+      if (expires && expires.toUTCString) {
+          options.expires = expires.toUTCString();
+      }
+
+      value = encodeURIComponent(value);
+
+      var updatedCookie = name + "=" + value;
+
+      for (var propName in options) {
+          updatedCookie += "; " + propName;
+          var propValue = options[propName];
+          if (propValue !== true) {
+              updatedCookie += "=" + propValue;
+          }
+       }
+
+      document.cookie = updatedCookie;
+}
+var password = document.getElementById('password');
+var button = document.getElementsByClass('btn-check')[0];
+
+button.addEventListener('click', setCookie('nick_name', password.value));
+*/
+/*
+
+let timerId = setTimeout(function tick() {
+
+  tinymce.triggerSave();
+  $.cookie('save',tinymce.activeEditor.getContent());
+  alert($.cookie('save'));
+
+
+  timerId = setTimeout(tick, 2000);
+}, 2000);
+*/
+
+
+function success() {
+   if(document.getElementById("continue-text-input").value==="") { 
+            document.getElementById('run-btn-continue').disabled = true; 
+        } else { 
+            document.getElementById('run-btn-continue').disabled = false;
+        }
+    }
